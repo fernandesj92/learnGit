2037c1af5e37629a8209fdb7b8e9a11df183a642
USER <USER@USER-PC>
2017-05-20 15:12:19 +0530

    third commit

--- src/helloGit.java
+++ src/helloGit.java
@@ -4,5 +4,6 @@ public class helloGit {
 	public static void main(String args[]){
 		System.out.println("This is git");
 		System.out.println("second output");
+		System.out.println("3rd commit");
 	}
 }
